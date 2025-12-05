bed90bc8b0bbc786feefe0b5684c0f13b8aea2f4
Dmitriy Khutornoy <thth13q@gmail.com>
2025-12-05 10:33:17 +0200

    img next config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'oahupsglz0lgjmbs.public.blob.vercel-storage.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   }
 }
