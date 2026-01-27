9863e4754e9003c0bfe503760d49163b774e1056
Dmitriy Khutornoy <thth13q@gmail.com>
2026-01-27 18:24:50 +0200

    hide feedbackwidget in mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -108,7 +108,9 @@ export default function RootLayout({
             <InitialLoader />
             {children}
             <SiteFooter />
-            <FeedbackWidget />
+            <div className="hidden sm:block">
+              <FeedbackWidget />
+            </div>
             <ConnectionDebug />
             <OfflineToast />
           </AuthProvider>
